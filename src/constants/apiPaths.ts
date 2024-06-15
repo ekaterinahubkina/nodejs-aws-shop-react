081c6f3da8a54ed5dbe248b1cc58143588d453b7
Kate Hubkina <ekaterinahubkina@yandex.by>
2024-06-16 01:31:11 +0300

    update products url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://sv7gf2fug2.execute-api.eu-central-1.amazonaws.com/prod",
+  product: "https://cdyw64p935.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
