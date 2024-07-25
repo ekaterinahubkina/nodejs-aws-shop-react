778a20a9e7e5d3ce5c52f57c551745e3d62f8016
Kate Hubkina <ekaterinahubkina@yandex.by>
2024-07-26 00:02:17 +0300

    update cart service api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://bne9kotxl5.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://localhost:4000/api",
+  cart: "https://efe74dtc53.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
