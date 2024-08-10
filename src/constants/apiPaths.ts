3fe570cbdaa34dbb4215852277a8f1acb3c1a7a2
Kate Hubkina <ekaterinahubkina@yandex.by>
2024-08-11 01:42:38 +0300

    use bff path for product and cart, use image from the db

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://cdyw64p935.execute-api.eu-central-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://bne9kotxl5.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://x7q4i11dkj.execute-api.eu-central-1.amazonaws.com",
   cart: "https://x7q4i11dkj.execute-api.eu-central-1.amazonaws.com/api",
 };
 
